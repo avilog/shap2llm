db1791974f36b436932954ef6e310964c9776a43
avilog <avilog@gmail.com>
2024-11-07 07:28:31 -0500

    Update explainer.py

--- shap2llm/explainer.py
+++ shap2llm/explainer.py
@@ -12,11 +12,6 @@ def encode_image(image):
     image.seek(0)
     return base64.b64encode(image.read()).decode('utf-8')
 
-# Function to encode the image
-def encode_image_path(image_path):
-  with open(image_path, "rb") as image_file:
-    return base64.b64encode(image_file.read()).decode('utf-8')
-
 class Explainer:
     def __init__(
         self,
